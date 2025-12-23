6dd77eea763180b61ffb45ef50de391fc4effbcf
KeamKRUB <pawat@MSI.localdomain>
2025-12-24 01:07:10 +0700

    add 2 views and 2 controllers

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,27 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProjectsController;
+use App\Http\Controllers\LabsController;
 
 Route::get('/', function () {
-    return view('welcome');
+		return view('index');
 });
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/projects', function () {
+    return view('projects');
+});
+Route::get('/labs', function () {
+    return view('labs');
+});
+
+Route::get('/labs', [LabsController::class, 'index']);
+
+Route::get('/projects/{title}', [ProjectsController::class, 'show']);
+
+Route::get('/projects', [ProjectsController::class, 'index']);
 
 Route::get('/about', [AboutController::class, 'index']);
 
